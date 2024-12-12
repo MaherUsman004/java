91bcdfc643fb5851a7dea6d3a4e7870c70f32ad2
MaherUsman004 <155241071+MaherUsman004@users.noreply.github.com>
2024-12-12 10:44:52 +0500

    Create area.js

---
+++ area.js
@@ -0,0 +1,13 @@
+function display(){
+    event.preventDefault();
+    let I1 = document.getElementById("length").value;
+    let I2 = document.getElementById("width").value;
+
+    let I3=I1*I2;
+    // alert(I3)
+     document.getElementById("area").value=I3;
+}
+function show()
+{
+    alert("Hello world")
+}
